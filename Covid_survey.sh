4fb7e20b7bcffe080fc0716a638099b239e6d3d1
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2021-09-24 12:30:32 +0200

    Update Covid_survey.sh

--- Covid_survey.sh
+++ Covid_survey.sh
@@ -1,4 +1,1 @@
-nextflow run nf-core/viralrecon --input /mnt/nas/analysis/210914-covid/sample_sheet.csv --outdir . --platform illumina 
---protocol amplicon --genome 'MN908947.3' --primer_set artic --primer_set_version 3  --skip_picard_metrics -r 2.2 -prof
-ile docker --skip_assembly --skip_kraken2 --skip_fastqc --skip_fastp  --skip_picard_metrics  --skip_multiqc --skip_mosd
-epth --skip_nextclade --max_memory 200.GB -c costum.config
+nextflow run nf-core/viralrecon --input sample_sheet.csv --outdir . --platform illumina --protocol amplicon --genome 'MN908947.3' --primer_set artic --primer_set_version 3  --skip_picard_metrics -r 2.2 -profile docker --skip_assembly --skip_kraken2 --skip_fastqc --skip_fastp  --skip_picard_metrics  --skip_multiqc --skip_mosdepth --skip_nextclade --max_memory 200.GB -c costum.config
