87c191aaf84d8a408e08efd061b854c90e176a23
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2021-10-18 11:38:28 +0200

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,8 +1,7 @@
-#/bin/bash
-#pass output dir of nf-core a parameter
-#find results in final inside output dir
+out=$1
 
-survey=$PWD/$1
-echo 'sample,taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version,pangolin_version,pangoLEARN_versio>
+echo 'sample,taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version
+,pangolin_version,pangoLEARN_version,pango_version,status,note' > pangolin.csv
 for i in $survey/variants/ivar/pangolin/*pangolin.csv;
-do line=$(head -n 2 $i | tail -1); s=$(sed 's/,.*//' <<< $line) ; sample_id=$(sed 's/Consensus_*//' <<< $s |  sed 's/.consensus.*//'); echo >
+do line=$(head -n 2 $i | tail -1); s=$(sed 's/,.*//' <<< $line) ; sample_id=$(sed 's/Consensus_*//' <<< $s |  sed 's/.consensus.*//'); echo $sample_id',
+'$line >> $survey/$1/pangolin.csv; done
