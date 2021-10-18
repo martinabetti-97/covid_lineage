f941f6682fab07a3ead9a515fa65c8257edc93dc
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2021-10-18 11:38:50 +0200

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,7 +1,5 @@
 out=$1
 
-echo 'sample,taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version
-,pangolin_version,pangoLEARN_version,pango_version,status,note' > pangolin.csv
+echo 'sample,taxon,lineage,conflict,ambiguity_score,scorpio_call,scorpio_support,scorpio_conflict,version,pangolin_version,pangoLEARN_version,pango_version,status,note' > pangolin.csv
 for i in $survey/variants/ivar/pangolin/*pangolin.csv;
-do line=$(head -n 2 $i | tail -1); s=$(sed 's/,.*//' <<< $line) ; sample_id=$(sed 's/Consensus_*//' <<< $s |  sed 's/.consensus.*//'); echo $sample_id',
-'$line >> $survey/$1/pangolin.csv; done
+do line=$(head -n 2 $i | tail -1); s=$(sed 's/,.*//' <<< $line) ; sample_id=$(sed 's/Consensus_*//' <<< $s |  sed 's/.consensus.*//'); echo $sample_id','$line >> $survey/$1/pangolin.csv; done
